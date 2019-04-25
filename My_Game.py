87851f8013a9f349d6c5a8665f2ac193574f77fd
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-04-25 15:05:19 -0400

    added ASCII art

--- My_Game.py
+++ My_Game.py
@@ -14,15 +14,63 @@ cyclops = Monster('Ted, the cyclops', 20, 9)
 
 
 def main():
-    goblin1.attack(vasa)
-    vasa.hero_attack(goblin1)
-    vasa.print_status()
-    goblin1.print_status()
-    vasa.take('Loot')
-    vasa.get_inventory()
-    vasa.point_calculator()
+    print('1. cyclops')
+    print("2. goblin")
+    print('3. wizard')
+    print("4. snail")
+    print("5. troll")
+    user_imput = input()
+    if user_imput == "1":
+        print("there is a big ugly cyclops in front of Vasalisa")
+        print(".-'  ____  `-.")
+        print("( ( (_()_)  ) ")
+        print("`-.   ^^   .-'")
+        print("   `._==_.'")
+        print("    __)(___)" )
 
-    vasa.eat('apple')
-    vasa.print_status()
+
+    elif user_imput == "2":
+        print("There is an ugly goblin in front of Vasalisa")
+        print("|\  \/      \/  /|")
+        print("| \ / =.  .= \ / |")
+        print("\( \   o\/o   / )/")
+        print(" \_, '-/  \-' ,_/")
+        print("   /   \__/   \  ")
+        print("   \,___/\___,/  ")
+        print(" ___\ \|--|/ /___")
+        print("     \      /    ")
+        print("      '----'       ")
+
+    elif user_imput == "3":
+        print("The Evil Wizard stand before Vasalisa")
+        print(" |\          .(' *) ' .")
+        print(" | \        ' .*) .'*")
+        print(" |(*\      .*(// .*) .")
+        print(" |___\       // (. '*")
+        print(" (((\"'\     // '  * .")
+        print(" ((c'7')   //)")
+        print(" ((((^))  /  |")
+        print("-')))(((-'/")
+        print(" (((()) __/'")
+        print("  )))( |")
+        print("  (()")
+
+    elif user_imput == "4":
+        print("There is a giant snail in front of Vasalisa")
+        print("     .----.   @   @ ")
+        print("   / .-\"-.`.   \ /")
+        print("   | | ('\ \ \_/ )")
+        print("  ,-\ `-.' /.'  /")
+        print(" '---`----'----'")
+
+    elif user_imput == '5':
+        print("There is an ugly troll in front of Vasalisa")
+        print(" __,='`````'=/__")
+        print(" '// (o) \(o) \ `' ")
+        print("//|     ,_)   (`\ ")
+        print("~~~\  `'==='  /-, ")
+        print("      `----'     `")    
+
+  
 
 main()    
