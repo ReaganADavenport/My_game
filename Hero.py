f9188161b361faec86fc1df658d3646d4229325f
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-04-26 22:03:29 -0400

    commented out unused methods

--- Hero.py
+++ Hero.py
@@ -5,11 +5,15 @@ class Hero(Warrior):
     def __init__(self, name, health, power):
         super().__init__(name, health, power)
         self.points = 0
-        self.inventory = ['sword', 'staff']
+        #self.inventory = ['sword', 'staff']
 
 
     def use_pen(self, enemy):
         enemy.health = enemy.health - (self.power + 10)
+        if enemy.alive() != True:
+            self.points = self.points + 40
+            self.health = self.health + 5
+            self.power = self.power + 5
 
 
     def hero_attack(self, enemy):
@@ -21,13 +25,13 @@ class Hero(Warrior):
             return self.points, self.health, self.power
             
 
-    def magic(self, other_person):
-        other_person.health = other_person.health - self.power
-        if other_person.alive() != True:
-            self.points = self.points + 20
-            self.health = self.health + 5
-            self.power = self.power + 5
-            return self.points, self.health, self.power
+    # def magic(self, other_person):
+    #     other_person.health = other_person.health - self.power
+    #     if other_person.alive() != True:
+    #         self.points = self.points + 20
+    #         self.health = self.health + 5
+    #         self.power = self.power + 5
+    #         return self.points, self.health, self.power
 
     def point_calculator(self):
         return self.points
@@ -35,9 +39,10 @@ class Hero(Warrior):
     def eat(self, food):
         self.health = self.health + 5
         return self.health
-
+    """
     def take(self, item):
         self.inventory.append(item)
 
     def get_inventory(self):
         print(self.inventory)
+    """
